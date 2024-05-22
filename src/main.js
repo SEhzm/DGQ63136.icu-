ce7e2885127b3dd93a133207094452e702040fdf
SE小何 <155219699+SEhzm@users.noreply.github.com>
2024-05-23 01:00:59 +0800

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,38 +1,44 @@
-import { createApp } from 'vue'  
-import App from './App.vue'  
-import router from './router'  
-import ElementPlus from 'element-plus'  
-import zhCn from 'element-plus/dist/locale/zh-cn.mjs'  
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'  
-  
-import '@/assets/css/global.css'  
-  
-const app = createApp(App)  
-  
-app.use(router)  
-app.use(ElementPlus, {  
-  locale: zhCn,  
-})  
-  
+import {
+  createApp
+} from 'vue'
+import App from './App.vue'
+import router from './router'
+import ElementPlus from 'element-plus'
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import '@/assets/css/global.css'
+
+const app = createApp(App)
+
+app.use(router)
+app.use(ElementPlus, {
+  locale: zhCn,
+})
+
 // 设置全局定时器
-setInterval(() => {  
+setInterval(() => {
   location.reload(); // 刷新整个页面  
 }, 3600000); //设置时间，ms
-  
-app.mount('#app')  
-  
-for (const [key, component] of Object.entries(ElementPlusIconsVue)) {  
-  app.component(key, component)  
+
+app.mount('#app')
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
 }
+
+import AV from 'leancloud-storage';
+
+const appId = 'G8sstSnHYpUQ8Lj1Y8BRHHb7-MdYXbMMI';
+const appKey = '1NOhe8Diw2jD9xzrPHv3r386';
+AV.init({ appId, appKey });
+
 
 
 //百度统计API
 var _hmt = _hmt || [];
-(function() {
+(function () {
   var hm = document.createElement("script");
   hm.src = "https://hm.baidu.com/hm.js?b9634ebcad194c62131c24600b2e1b10";
   var s = document.getElementsByTagName("script")[0];
   s.parentNode.insertBefore(hm, s);
 })();
-
-

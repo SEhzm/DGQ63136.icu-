79251315661bfe752628f2de5ff8e2d381847158
hzming1 <13068475+hzming1@user.noreply.gitee.com>
2024-06-24 15:12:21 +0800

    2024/6/24修改

--- springboot/src/main/java/com/dgq63136/springboot/controller/WebController.java
+++ springboot/src/main/java/com/dgq63136/springboot/controller/WebController.java
@@ -23,13 +23,14 @@ public class WebController {
     @GetMapping("/allBarrage/Page")
     public Result allBarrage(){
         List<Barrage> barrageList = webService.allPage();
+        log.info("进入首页");
         return Result.success(barrageList);
     }
     @GetMapping("/JZCM/Page")
     public Result JZCM(@RequestParam(defaultValue = "1") Integer pageNum,
                        @RequestParam(defaultValue = "15") Integer pageSize){
 
-        log.info("分页数据：{},{}",pageNum,pageSize);
+        log.info("警钟长鸣 分页数据：{},{}",pageNum,pageSize);
 
         PageInfo<Barrage> pageInfo = webService.JZCMPage(pageNum,pageSize);
         return Result.success(pageInfo);
@@ -39,7 +40,7 @@ public class WebController {
     public Result JZCM2022(@RequestParam(defaultValue = "1") Integer pageNum,
                        @RequestParam(defaultValue = "15") Integer pageSize){
 
-        log.info("分页数据：{},{}",pageNum,pageSize);
+        log.info("2022警钟长鸣 分页数据：{},{}",pageNum,pageSize);
 
         PageInfo<Barrage> pageInfo = webService.J2022Page(pageNum,pageSize);
         return Result.success(pageInfo);
@@ -49,7 +50,7 @@ public class WebController {
     public Result JZCM2023(@RequestParam(defaultValue = "1") Integer pageNum,
                        @RequestParam(defaultValue = "15") Integer pageSize){
 
-        log.info("分页数据：{},{}",pageNum,pageSize);
+        log.info("2023警钟长鸣 分页数据：{},{}",pageNum,pageSize);
 
         PageInfo<Barrage> pageInfo = webService.J2023Page(pageNum,pageSize);
         return Result.success(pageInfo);
@@ -59,7 +60,7 @@ public class WebController {
     public Result JZCM2024(@RequestParam(defaultValue = "1") Integer pageNum,
                        @RequestParam(defaultValue = "15") Integer pageSize){
 
-        log.info("分页数据：{},{}",pageNum,pageSize);
+        log.info("2024警钟长鸣 分页数据：{},{}",pageNum,pageSize);
 
         PageInfo<Barrage> pageInfo = webService.J2024Page(pageNum,pageSize);
         return Result.success(pageInfo);
@@ -69,7 +70,7 @@ public class WebController {
     public Result p1(@RequestParam(defaultValue = "1") Integer pageNum,
                        @RequestParam(defaultValue = "50") Integer pageSize){
 
-        log.info("分页数据：{},{}",pageNum,pageSize);
+        log.info("+1页 分页数据：{},{}",pageNum,pageSize);
 
         PageInfo<Barrage> pageInfo = webService.p1Page(pageNum,pageSize);
         return Result.success(pageInfo);
@@ -79,7 +80,7 @@ public class WebController {
     public Result ruibin(@RequestParam(defaultValue = "1") Integer pageNum,
                        @RequestParam(defaultValue = "15") Integer pageSize){
 
-        log.info("分页数据：{},{}",pageNum,pageSize);
+        log.info("瑞斌页 分页数据：{},{}",pageNum,pageSize);
 
         PageInfo<Barrage> pageInfo = webService.ruibinPage(pageNum,pageSize);
         return Result.success(pageInfo);
@@ -89,7 +90,7 @@ public class WebController {
     public Result XTT(@RequestParam(defaultValue = "1") Integer pageNum,
                        @RequestParam(defaultValue = "15") Integer pageSize){
 
-        log.info("分页数据：{},{}",pageNum,pageSize);
+        log.info("XTT 分页数据：{},{}",pageNum,pageSize);
 
         PageInfo<Barrage> pageInfo = webService.XTTPage(pageNum,pageSize);
         return Result.success(pageInfo);
@@ -99,7 +100,7 @@ public class WebController {
     public Result DGQ(@RequestParam(defaultValue = "1") Integer pageNum,
                        @RequestParam(defaultValue = "15") Integer pageSize){
 
-        log.info("分页数据：{},{}",pageNum,pageSize);
+        log.info("DGQ 分页数据：{},{}",pageNum,pageSize);
 
         PageInfo<Barrage> pageInfo = webService.DGQPage(pageNum,pageSize);
         return Result.success(pageInfo);
@@ -109,7 +110,7 @@ public class WebController {
     public Result baizi(@RequestParam(defaultValue = "1") Integer pageNum,
                        @RequestParam(defaultValue = "15") Integer pageSize){
 
-        log.info("分页数据：{},{}",pageNum,pageSize);
+        log.info("白字 分页数据：{},{}",pageNum,pageSize);
 
         PageInfo<Barrage> pageInfo = webService.baiziPage(pageNum,pageSize);
         return Result.success(pageInfo);
@@ -119,7 +120,7 @@ public class WebController {
     public Result QUQU(@RequestParam(defaultValue = "1") Integer pageNum,
                        @RequestParam(defaultValue = "15") Integer pageSize){
 
-        log.info("分页数据：{},{}",pageNum,pageSize);
+        log.info("QUQU 分页数据：{},{}",pageNum,pageSize);
 
         PageInfo<Barrage> pageInfo = webService.QUQUPage(pageNum,pageSize);
         return Result.success(pageInfo);
